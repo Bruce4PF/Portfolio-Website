2064e0240eb3eb664a3cb86954a6a743dfcebcc0
Bruce4PF <bliu445@uwo.ca>
2025-06-08 23:28:48 -0400

    Fix Typo

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -81,7 +81,7 @@ const about = {
                 timeframe: 'May 2025 - September 2025',
                 role: 'Product Manager & GTM Intern',
                 achievements: [
-                    <>Owned Loop's Interac e-Transfer feature end-to-end, reducing deposit times by 99.99%% (2 days to 30 seconds), significantly improving user experience and reducing support tickets</>,
+                    <>Owned Loop's Interac e-Transfer feature end-to-end, reducing deposit times by 99.99% (2 days to 30 seconds), significantly improving user experience and reducing support tickets</>,
                     <>Launched an interactive product demo on Loop's website, leading to a 20% sales conversion increase by enabling users to explore Loop's platform</>,
                 ],
                 images: [ // optional: leave the array empty if you don't want to display images
